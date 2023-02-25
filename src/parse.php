8be7096122ae730a77ceb2e355b17c1462babc27
munenepeter <peternjega7@gmail.com>
2023-02-25 06:02:30 +0300

    remove duplication

--- src/parse.php
+++ src/parse.php
@@ -13,45 +13,35 @@ include 'functions.php';
 $messages = [];
 // Handle File upload
 if (!empty($_FILES) && empty($_POST['text'])) {
-    $start_time = microtime(true);
-    logger('INFO: Succesfully uploaded ' . json_encode($_FILES));
     try {
         $text = readUploadedFile($_FILES['files']['tmp_name']);
-        $end_time = microtime(true);
-        $execution_time = round(($end_time - $start_time), 4);
     } catch (\Exception $e) {
         logger('ERROR: ' . $e->getMessage());
         $messages['text'] = '<p class="p-4 text-red-500 text-sm font-semibold text-center">You have no idea what happened huh? Well so do I. Code:E214</p>';
     }
-
-    if (!empty($text)) {
-        logger("INFO: Successfully parsed " . $_FILES['files']['name'] . " in  $execution_time seconds!");
-        $messages['text'] = $text;
-    } else {
-        logger('ERROR: Couldn\'t get the text!');
-        $messages['lis_found'] = "No LI's Found!";
-        echo json_encode($messages);
-        return;
-    }
     // Handle Text input
 } elseif (!empty($_POST['text']) && empty($_FILES)) {
-
     $text = trim($_POST['text']);
-
-    if (!empty($text)) {
-        //logger("INFO: Successfully parsed " . $_FILES['files']['name'] . " in  $execution_time seconds!");
-        $messages['text'] = $text;
-    } else {
-        logger('ERROR: Couldn\'t get the text!');
-        $messages['lis_found'] = "No LI's Found!";
-        echo json_encode($messages);
-        return;
-    }
 }
+
+
+
+//check if text is empty
+if (!empty($text)) {
+    $messages['text'] = $text;
+} else {
+    logger('ERROR: Couldn\'t get the text!');
+    $messages['lis_found'] = "No LI's Found!";
+    echo json_encode($messages);
+    return;
+}
+
+
 //search for LIS
 if (!empty(getLisInText($text, getAllLis()))) {
     $messages['lis_found'] =  implode(", ", getLisInText($text, getAllLis()));
 } else {
+    $messages['text'] = "No text was found to be parsed!";
     $messages['lis_found'] = "No LI's Found!";
 }
 
@@ -60,19 +50,3 @@ echo json_encode($messages);
 clearstatcache();
 exit;
 
-if (isset($_GET['url'])) {
-    $url = urldecode($_GET['url']); // Decode URL-encoded string
-
-
-    // Use basename() function to return the base name of file
-    $file_name = basename($url);
-
-    // Use file_get_contents() function to get the file
-    // from url and use file_put_contents() function to
-    // save the file by using base name
-    if (file_put_contents($file_name, file_get_contents($url))) {
-        echo "File downloaded successfully";
-    } else {
-        echo "File downloading failed.";
-    }
-}
