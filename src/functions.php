1553dad6140f506e7dcb0529fe08127a4932f1d7
munenepeter <peternjega7@gmail.com>
2023-04-19 14:16:04 +0300

    updated cache

--- src/functions.php
+++ src/functions.php
@@ -18,21 +18,8 @@ function logger(String $message) {
 }
 
 function getAllLis() {
-    //from db
-    $databaseLis = json_decode(file_get_contents("https://munenepeter.github.io/my-file-tracker/data/datas.json"));
-
-    $allLis = [];
-
-    foreach ($databaseLis as $databaseLi) {
-        $allLis[] = $databaseLi->name;
-
-        $abbrs = explode(',', trim($databaseLi->abbr));
-
-        foreach ($abbrs as $abbr) {
-            $allLis[] = trim($abbr);
-        }
-    }
-    return  $allLis;
+    //from cached file
+    return json_decode(file_get_contents("static/lis.txt"));
 }
 
 
