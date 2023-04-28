61b4d8925248feb72cdfb19c32892cfd3171ab62
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-04-28 14:39:09 +0300

    optimize code

--- src/pearls.php
+++ src/pearls.php
@@ -5,24 +5,15 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST");
 
 include 'functions.php';
-//empty response
-$messages = [];
-
-
-
 
 $pearls = getAllKeywords();
 
 if (!empty($pearls)) {
     http_response_code(200);
-    $messages['pearls'] =  implode(", ", $pearls);
+    echo json_encode(['pearls' => $pearls], JSON_UNESCAPED_UNICODE);
 } else {
     http_response_code(500);
-    $messages['message'] = "Something is off and can't access the pearls!";
+    echo json_encode(['message' => "Something is off and can't access the pearls!"]);
 }
 
-//return the object response
-echo json_encode($messages, JSON_INVALID_UTF8_IGNORE);
-
-clearstatcache();
 exit;
