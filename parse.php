9ca720f2eb58db4035a92595960e1712038ef3d5
munenepeter <peternjega7@gmail.com>
2023-08-21 19:27:41 +0300

    no progress

--- parse.php
+++ parse.php
@@ -1,7 +1,7 @@
 <?php
 
 use Smalot\PdfParser\Parser;
- 
+
 include 'vendor/autoload.php';
 
 $filename = __DIR__ . '/test.pdf';
@@ -9,14 +9,22 @@ $filename = __DIR__ . '/test.pdf';
 
 
 $config = new \Smalot\PdfParser\Config();
-$config->setFontSpaceLimit(-60);
+$config->setFontSpaceLimit(01);
 $config->setRetainImageContent(true);
+$config->setPdfWhitespacesRegex('[\0\t\n\f\r ]');
 
 $parser = new \Smalot\PdfParser\Parser([], $config);
 
 $parser = new Parser();
 $pdf = $parser->parseFile($filename);
 
+$images = $pdf->getObjectsByType('XObject', 'Image');
+
+foreach ($images as $image) {
+    echo '<img src="data:image/jpg;base64,' . base64_encode($image->getContent()) . '" />';
+}
+
 echo '<pre>';
 
-echo $pdf->getText();
+print_r($pdf->getText());
+
