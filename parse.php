cb3253556f00afb49428987093847b929be4f49f
munenepeter <peternjega7@gmail.com>
2023-08-22 12:37:29 +0300

    almost there

--- parse.php
+++ parse.php
@@ -6,25 +6,30 @@ include 'vendor/autoload.php';
 
 $filename = __DIR__ . '/test.pdf';
 
-
+function nl2p($string) {
+    return "<p>" . implode("</p><p>", explode("\n", $string)) . "</p>";
+}
 
 $config = new \Smalot\PdfParser\Config();
-$config->setFontSpaceLimit(01);
+$config->setFontSpaceLimit(-80);
 $config->setRetainImageContent(true);
 $config->setPdfWhitespacesRegex('[\0\t\n\f\r ]');
 
 $parser = new \Smalot\PdfParser\Parser([], $config);
 
-$parser = new Parser();
+
 $pdf = $parser->parseFile($filename);
 
-$images = $pdf->getObjectsByType('XObject', 'Image');
 
-foreach ($images as $image) {
-    echo '<img src="data:image/jpg;base64,' . base64_encode($image->getContent()) . '" />';
-}
 
-echo '<pre>';
+print_r(nl2p($pdf->getText()));
 
-print_r($pdf->getText());
+//$parser = new Parser();
 
+// $images = $pdf->getObjectsByType('XObject', 'Image');
+
+// foreach ($images as $image) {
+//     echo '<img src="data:image/jpg;base64,' . base64_encode($image->getContent()) . '" />';
+// }
+
+//echo '<pre>';
