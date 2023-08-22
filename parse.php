98d59e53284455def223951f92b46dbaae6a1128
munenepeter <peternjega7@gmail.com>
2023-08-22 12:48:01 +0300

    almost there, remove <br>s

--- parse.php
+++ parse.php
@@ -1,13 +1,24 @@
 <?php
 
+
+
+
+ 
 use Smalot\PdfParser\Parser;
 
 include 'vendor/autoload.php';
 
 $filename = __DIR__ . '/test.pdf';
 
-function nl2p($string) {
-    return "<p>" . implode("</p><p>", explode("\n", $string)) . "</p>";
+function pdfTextToHtml($pdfText) {
+    $paragraphs = preg_split('/\s*\n\s*\n\s*/', $pdfText);
+    $html = '';
+    
+    foreach ($paragraphs as $paragraph) {
+        $html .= '<p>' . nl2br(htmlspecialchars($paragraph)) . '</p>';
+    }
+    
+    return $html;
 }
 
 $config = new \Smalot\PdfParser\Config();
@@ -16,13 +27,42 @@ $config->setRetainImageContent(true);
 $config->setPdfWhitespacesRegex('[\0\t\n\f\r ]');
 
 $parser = new \Smalot\PdfParser\Parser([], $config);
-
-
 $pdf = $parser->parseFile($filename);
 
+$extractedText = $pdf->getText();
+$htmlOutput = pdfTextToHtml($extractedText);
+
+echo trim(str_replace('<br>', '', $htmlOutput));
 
 
-print_r(nl2p($pdf->getText()));
+// include 'vendor/autoload.php';
+
+// $filename = __DIR__ . '/test.pdf';
+
+// function nl2p($string) {
+//     return "<p>" . implode("</p><p>", explode("\n", $string)) . "</p>";
+// }
+
+// function cleanText($text) {
+//     // Remove multiple spaces and line breaks
+//     //$text = preg_replace('/\s+/', ' ', $text);
+
+//     return $text;
+// }
+
+// $config = new \Smalot\PdfParser\Config();
+// $config->setFontSpaceLimit(-80);
+// $config->setRetainImageContent(true);
+// $config->setPdfWhitespacesRegex('[\0\t\n\f\r ]');
+
+// $parser = new \Smalot\PdfParser\Parser([], $config);
+
+
+// $pdf = $parser->parseFile($filename);
+
+// $text = cleanText($pdf->getText());
+
+// echo (nl2p($text));
 
 //$parser = new Parser();
 
