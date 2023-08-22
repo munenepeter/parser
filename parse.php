7cede70e1b6df278e005986f1b8436206f55ec71
munenepeter <peternjega7@gmail.com>
2023-08-22 14:46:13 +0300

    clean up

--- parse.php
+++ parse.php
@@ -1,9 +1,5 @@
 <?php
 
-
-
-
- 
 use Smalot\PdfParser\Parser;
 
 include 'vendor/autoload.php';
@@ -101,30 +97,9 @@ $htmlOutput = pdfTextToHtml($extractedText);
 //echo '<pre>';
 echo combineParagraphs($htmlOutput);
 
-
-// include 'vendor/autoload.php';
-
-// $filename = __DIR__ . '/test.pdf';
-
-// function nl2p($string) {
-//     return "<p>" . implode("</p><p>", explode("\n", $string)) . "</p>";
-// }
-
  
-
-// $config = new \Smalot\PdfParser\Config();
-// $config->setFontSpaceLimit(-80);
-// $config->setRetainImageContent(true);
-// $config->setPdfWhitespacesRegex('[\0\t\n\f\r ]');
-
-// $parser = new \Smalot\PdfParser\Parser([], $config);
-
-
-// $pdf = $parser->parseFile($filename);
-
-// $text = cleanText($pdf->getText());
-
-// echo (nl2p($text));
+ 
+ 
 
 //$parser = new Parser();
 
@@ -134,4 +109,4 @@ echo combineParagraphs($htmlOutput);
 //     echo '<img src="data:image/jpg;base64,' . base64_encode($image->getContent()) . '" />';
 // }
 
-//echo '<pre>';
+ 
